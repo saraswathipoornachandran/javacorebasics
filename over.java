66caeb7eb56405ae43c4988f0d819002143f6927
saraswathipoornachandran <sarachandran89@gmail.com>
2018-05-30 16:27:26 -0700

    overloading

---
+++ over.java
@@ -0,0 +1,12 @@
+package com.basic;
+
+public class over {
+public static void main(String args[]) {
+}
+public boolean isMultiple(int one,int two){
+return two % one == 0 ? true : false;	
+}
+public boolean isEven(int one){
+	return one % 2 == 0 ? true : false;	
+}
+}
